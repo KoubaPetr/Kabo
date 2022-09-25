5f855316cd849c9452e349999f1e2bbcb83e35c7
Kouba, Petr <koubape9@fel.cvut.cz>
2022-09-25 15:47:33 +0200

    started working on the GUI - not linked to engine yet, updated graphics

--- src/kabo_gui.py
+++ src/kabo_gui.py
@@ -1,6 +1,22 @@
 import pygame
+from graphics_config import CARD_BACK_IMAGE_PATH_SCRIBBLE
 
 pygame.init()
 bounds = (1024, 768)
 window = pygame.display.set_mode(bounds)
 pygame.display.set_caption("Kabo")
+
+cardBack = pygame.image.load(CARD_BACK_IMAGE_PATH_SCRIBBLE)
+cardBack = pygame.transform.scale(cardBack, (int(238 * 0.8), int(332 * 0.8)))
+
+
+def render_game(window):
+    window.fill((15, 169, 0))
+    # font = pygame.font.SysFont("arial", 60, True)
+    window.blit(cardBack, (100, 200))
+
+
+pygame.display.update()
+render_game(window)
+pygame.display.update()
+print("wait")
